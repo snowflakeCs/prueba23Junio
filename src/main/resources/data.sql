a51d3117fbc3430b34eccf27f58120c397617b98
snowflakeCs <constanza.vlm@hotmail.com>
2023-06-24 17:11:17 -0400

    fix 2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+/*CREATE TABLE music_style (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  vote_count INT
+);
+INSERT INTO music_style (id, name, vote_count) VALUES (1, 'Rock', 0);
+INSERT INTO music_style (id, name, vote_count) VALUES (2, 'Jazz', 0);
+INSERT INTO music_style (id, name, vote_count) VALUES (3, 'Pop', 0);*/
